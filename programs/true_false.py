916c2a9d393a1dacb1d48b20faacc6ca0d862844
ilseku <54395297+ilseku@users.noreply.github.com>
2020-12-12 20:05:28 +0100

    fixing some plots

--- programs/true_false.py
+++ programs/true_false.py
@@ -5,12 +5,13 @@ from sklearn.datasets import load_breast_cancer
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
 import seaborn as sns
 import xgboost as xgb
 
 np.random.seed(40)
 
-def true_false_map(predictions, y_test):
+def true_false_map(predictions, y_test, method):
     true_positive = 0
     true_negative = 0
     false_positive = 0
@@ -30,6 +31,7 @@ def true_false_map(predictions, y_test):
     results = pd.DataFrame(results, index=['True', 'False'], columns=['Negative', 'Positive'])
     print(results)
     sns.heatmap(results, annot=True)
+    plt.title(method)
     plt.show()
 
 
@@ -44,30 +46,88 @@ cancer = load_breast_cancer()
 X_train, X_test, y_train, y_test = train_test_split(cancer.data,cancer.target,random_state=0)
 print(X_train.shape)
 print(X_test.shape)
+
 # Logistic Regression
-logreg = LogisticRegression(solver='newton-cg')
+logreg = LogisticRegression(solver='sag', max_iter=5000)
 logreg.fit(X_train, y_train)
-print("Test set accuracy with Logistic Regression: {:.2f}".format(logreg.score(X_test,y_test)))
+print("Test set accuracy with Logistic Regression: {:.3f}".format(logreg.score(X_test,y_test)))
 
 predictions = logreg.predict(X_test)
 
-#true_false_map(predictions, y_test)
+
+true_false_map(predictions, y_test, 'Logistic Regression')
+
 
 # Decision Trees
 deep_tree_clf = DecisionTreeClassifier(max_depth=None)
 deep_tree_clf.fit(X_train, y_train)
-print("Test set accuracy with Decision Trees: {:.2f}".format(deep_tree_clf.score(X_test,y_test)))
+print("Test set accuracy with Decision Trees: {:.3f}".format(deep_tree_clf.score(X_test,y_test)))
 
 predict_tree = deep_tree_clf.predict(X_test)
 
-true_false_map(predict_tree, y_test)
+true_false_map(predict_tree, y_test, 'Decision Tree')
 
 #xgboost
 
 xg_clf = xgb.XGBClassifier()
 xg_clf.fit(X_train,y_train)
-print("Test set accuracy with XGBoost: {:.2f}".format(xg_clf.score(X_test,y_test)))
+print("Test set accuracy with XGBoost: {:.3f}".format(xg_clf.score(X_test,y_test)))
 
 predict_xgb = xg_clf.predict(X_test)
 
-true_false_map(predict_xgb, y_test)
+true_false_map(predict_xgb, y_test, 'XG-boost')
+
+svm_clf = SVC()
+svm_clf.fit(X_train, y_train)
+predict_svm = svm_clf.predict(X_test)
+print("Test set accuracy with SVM: {:.3f}".format(svm_clf.score(X_test,y_test)))
+
+true_false_map(predict_svm, y_test, 'Support Vector Machine')
+
+
+
+#now we use scaled data
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+scaler.fit(X_train)
+X_train_scaled = scaler.transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+
+# Logistic Regression
+logreg = LogisticRegression(solver='sag', max_iter=500)
+logreg.fit(X_train_scaled, y_train)
+print("Test set accuracy with Logistic Regression scaled: {:.3f}".format(logreg.score(X_test_scaled,y_test)))
+
+predictions = logreg.predict(X_test_scaled)
+
+
+#true_false_map(predictions, y_test, 'Logistic Regression scaled')
+
+
+# Decision Trees
+deep_tree_clf = DecisionTreeClassifier(max_depth=None)
+deep_tree_clf.fit(X_train_scaled, y_train)
+print("Test set accuracy with Decision Trees scaled: {:.3f}".format(deep_tree_clf.score(X_test_scaled,y_test)))
+
+predict_tree = deep_tree_clf.predict(X_test_scaled)
+
+#true_false_map(predict_tree, y_test, 'Decision Tree scaled')
+
+#xgboost
+
+xg_clf = xgb.XGBClassifier()
+xg_clf.fit(X_train_scaled,y_train)
+print("Test set accuracy with XGBoost scaled: {:.3f}".format(xg_clf.score(X_test_scaled,y_test)))
+
+predict_xgb = xg_clf.predict(X_test_scaled)
+
+#true_false_map(predict_xgb, y_test, 'XG-boost scaled')
+
+svm_clf = SVC()
+svm_clf.fit(X_train_scaled, y_train)
+print("Test set accuracy with SVM scaled: {:.3f}".format(svm_clf.score(X_test_scaled,y_test)))
+
+predict_svm = svm_clf.predict(X_test_scaled)
+
+#true_false_map(predict_svm, y_test, 'Support Vector Machine scaled')
